e7d3029c067460f57699229048955c77f52b2d33
tingwei.chiang <tingwei.chiang@f5b6643f-46c0-4d26-b42e-b4675865fad5>
2016-09-12 08:36:58 +0000

    1. [新增] 帳號密碼權限管理
    git-svn-id: https://svn.gorilla-technology.com:1081/svn/IVSSystem/IVAR-SERVER/trunk/AccountServer@31282 f5b6643f-46c0-4d26-b42e-b4675865fad5

---
+++ UserInfo.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "AccountDefine.h"
+
+using json = nlohmann::json;
+
+namespace gorilla {
+    namespace account{
+
+        class AccountDB;;
+        class User
+        {
+        public:
+            
+            User(std::shared_ptr<AccountDB> sp_account_db, const std::string& str_user_info);
+
+            ~User();
+
+            std::string Account() const;
+            
+            std::string Password() const;
+
+            std::string EncryptedPassword() const;
+
+            std::string Level() const;
+
+            json Json() const;
+        
+            std::string JsonString() const;
+            
+            bool AddUser(int& out_n_sql_error);
+
+            std::string UpdateUser(const std::string &str_user_info);
+
+            bool DeleteUser();
+                
+        private:
+
+            std::shared_ptr<AccountDB> m_sp_account_db;
+            json m_json_response_user_info;
+                
+            std::string m_str_account;
+            std::string m_str_password;
+            std::string m_str_encrypted_password;
+            std::string m_str_level;
+            std::string m_str_description;
+
+            bool IsInfoExsist(const json& j_user_info, const std::string& str_key,
+                std::string& out_srt_info);
+
+            void SetUserInfo(const std::string& str_user_info);
+
+            void SetResponseUserInfo();
+        };
+    }
+}
+
+
+
