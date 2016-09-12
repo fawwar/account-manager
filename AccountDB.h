3127f16e6c51726c4174e4258ceec2597095d0fa
tingwei.chiang <tingwei.chiang@f5b6643f-46c0-4d26-b42e-b4675865fad5>
2016-09-12 08:36:55 +0000

    1. [新增] 帳號密碼權限管理
    git-svn-id: https://svn.gorilla-technology.com:1081/svn/IVSSystem/IVAR-SERVER/trunk/AccountServer@31266 f5b6643f-46c0-4d26-b42e-b4675865fad5

---
+++ AccountDB.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <mutex>
+#include <map>
+#include <list>
+
+#include "./import/json-develop/json.hpp"
+
+// Forward declaration
+struct sqlite3;
+typedef struct sqlite3 SQLiteDB;
+using json = nlohmann::json;
+
+namespace gorilla {
+    namespace account{
+
+        class AccountDB
+        {
+        public:
+
+            enum TableName 
+            {
+                USERS,
+                LEVELS
+            };
+       
+            static std::shared_ptr<AccountDB>& Instance();
+            
+            ~AccountDB();
+
+            bool GetAllItems(TableName e_table_name, std::list<std::string>& out_lst_json_string);
+
+            bool Insert(TableName e_table_name, const json& json_obj, int& out_n_sql_error);
+
+            bool Update(TableName e_table_name, const std::string& str_update_key_field,
+                 const std::string& str_update_key_value, const json& json_obj);
+
+            bool Delete(TableName e_table_name, const std::string& str_delete_key_field,
+                 const std::string& str_delete_key_value);
+
+        private:
+
+            enum CommandType{INSERT_FIELD, INSERT_VALUE};
+            
+            const std::string m_strDBFilePath;
+            static std::mutex g_mutexInstance;
+            mutable std::mutex m_muxSQLDB;
+            SQLiteDB* m_pSQLDB;
+            std::map<std::string, SQLiteDB*> m_mapDBTable;
+
+            AccountDB();
+
+            AccountDB(const AccountDB& rhs);
+
+            bool OpenDB();
+            
+            void CloseDB(SQLiteDB* pSQLiteDB);
+
+            void CreateTable(SQLiteDB* p_SQLDB, const std::string& str_table_name);
+
+            void SetSQLInsertTableCmd(const json& json_obj, CommandType e_cmd_type,
+                char*& out_cmd);
+
+            void SetSQLUpdatetTableCmd(const json& json_obj, CommandType e_cmd_type,
+                char*& out_cmd);
+     
+        };
+    }
+}
+
