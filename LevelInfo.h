5adeb916ad5620561cbee7e963431172d0c05dd3
tingwei.chiang <tingwei.chiang@f5b6643f-46c0-4d26-b42e-b4675865fad5>
2016-09-12 08:36:57 +0000

    1. [新增] 帳號密碼權限管理
    git-svn-id: https://svn.gorilla-technology.com:1081/svn/IVSSystem/IVAR-SERVER/trunk/AccountServer@31275 f5b6643f-46c0-4d26-b42e-b4675865fad5

---
+++ LevelInfo.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <list>
+
+#include "AccountDefine.h"
+
+using json = nlohmann::json;
+
+namespace gorilla {
+    namespace account{
+
+        class AccountDB;;
+        class Level
+        {
+        public:
+            
+            Level(std::shared_ptr<AccountDB> sp_account_db, const std::string& str_level_info);
+
+            ~Level();
+
+            std::string LevelName() const;
+
+            json Json() const;
+        
+            std::string JsonString() const;
+
+            bool AddLevel(int& out_n_sql_error);
+
+            std::string Features(const std::list<std::string>& lst_fields) const;
+
+            std::string UpdateLevel(const std::string &str_level_info);
+
+            bool DeleteLevel();
+            
+        private:
+
+            std::shared_ptr<AccountDB> m_sp_account_db;
+            json m_json_level_info;
+
+            std::string m_str_levelName;
+            std::string m_str_features;
+            std::string m_str_description;
+
+            void SetLevelInfo(const std::string& str_level_info);
+        };
+    }
+}
+
