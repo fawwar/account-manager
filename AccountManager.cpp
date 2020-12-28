9b80146a6e857907cf04ceba6b792409690ada1a
Denny Wang <dennywang@gorilla-technology.com>
2020-12-28 15:59:00 +0800

    modify UpdateUser non-admin can not update self AccessRightName

--- AccountManager.cpp
+++ AccountManager.cpp
@@ -442,21 +442,22 @@ namespace gorilla {
    
                     /* user account only change password */
 			
-                    if(IsKeyExsist(info, "account") || IsKeyExsist(info, "accessRightName")){
-
-                        //out_str_reply = m_error_reply.GetError("User No Permissions To Chang Account Or AccessRightName",
-                        //    "<AccountManager::UpdateUser> FORBIDDEN");
+                    if(  IsKeyExsist(info, "accessRightName")){
+                        // IsKeyExists(info, "account") || 
+                        out_str_reply = m_error_reply.GetError("User No Permissions To Chang Account Or AccessRightName",
+                            "<AccountManager::UpdateUser> FORBIDDEN");
                         
-                        //return FORBIDDEN;
+                        return FORBIDDEN;
                     }
                     
                     /* check password vaild */
-                    else{
+		
+                   // else{
                     	if (!IsPasswordVaild(str_user_info, out_str_reply)){
                             out_str_reply = m_error_reply.GetError("User Password Invaild","<AccountManager::UpdateUser> FORBIDDEN"); 
                             return FORBIDDEN;
                         }
-		   }
+		  // }
                     
                     errorCode = SUCCESS_RESPONSE;   
                     out_str_reply = it->second->UpdateUser(str_user_info);
