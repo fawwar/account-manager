3fe20de1a1db06f5aa6aa5fc6391c1c258645b91
Denny Wang <dennywang@gorilla-technology.com>
2022-01-06 06:39:04 +0000

    [gitlab][ci] modify delivery.py arg count

--- scripts/delivery.py
+++ scripts/delivery.py
@@ -110,8 +110,8 @@ def getProject(argv):
     global PROJECT
     PROJECT = 'std'
     if len(argv) > 1 :
-        if str(argv[2]) == 'bi' or str(argv[2]) == 'telstra':
-            PROJECT = str(argv[2])
+        if str(argv[1]) == 'bi' or str(argv[1]) == 'telstra':
+            PROJECT = str(argv[1])
                    
     print('Project ',PROJECT)
 
