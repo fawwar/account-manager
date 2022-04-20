e04bda1dc020185210a530e8836b47e339a734da
Denny Wang <dennywang@gorilla-technology.com>
2022-04-20 06:32:54 +0000

    [gitlab][ci] annotation packaging.py compatibility modify build.py

--- scripts/packaging.py
+++ scripts/packaging.py
@@ -10,7 +10,6 @@ from pathlib import Path
 from datetime import datetime
 import re
 import xml.etree.ElementTree as ET
-import numpy as np
 
 PROJECT=""
 VERSION=""
@@ -35,83 +34,94 @@ def run(command, cb= sys.stdout.buffer.write):
         raise SystemExit(rc)
 
 def packaging():
-    if os.name == 'nt':
-        print('win-x86_64')
-        os.chdir(rootPath)
-        if (os.path.isfile('X:\\')):
-            print('X:\\ file exist')
-            run('net use "X:" /delete /y')
-        # compatibility
-        #qtAssignPath = os.path.join('X:\\' ,PROJECT, 'win-x86_64')
-        if os.getenv('CI_COMMIT_TAG'):
-            print('Release build')
-            regExpr(os.environ['CI_COMMIT_TAG'])
-            projPath = os.path.join('X:\\','build' , PROJECT, VERSION, 'win-x86_64')
-            winCMD = 'net use /y "X:" "\\\\%SMB_URL%\\IOT-Release\\'+ SERVICE +'" /u:"GORILLASCIENCE\\%SMB_USERNAME%" %SMB_PASSWORD%'
-
+    try:
+        if os.name == 'nt':
+            print('win-x86_64')
+            os.chdir(rootPath)
+            if (os.path.isfile('X:\\')):
+                print('X:\\ file exist')
+                run('net use "X:" /delete /y')
             # compatibility
-            qtAssignPath = os.path.join('X:\\' ,PROJECT, 'win-x86_64')
-            WinCMD = 'net use /y "X:" "\\\\%SMB_URL%\\IOT-Release\\'+ SERVICE +'" /u:"GORILLASCIENCE\\%SMB_USERNAME%" %SMB_PASSWORD%'
-            run(WinCMD)
-            if not (os.path.isdir(qtAssignPath)):
-                os.makedirs(qtAssignPath, mode=0o755, exist_ok=True)
-            shutil.copy2(rootPath.joinpath(SERVICE+'.zip'), qtAssignPath)
-            print('copy file ', qtAssignPath)
-            run('net use "X:" /delete /y')
+            #qtAssignPath = os.path.join('X:\\' ,PROJECT, 'win-x86_64')
+            if os.getenv('CI_COMMIT_TAG'):
+                print('Release build')
+                regExpr(os.environ['CI_COMMIT_TAG'])
+                winCMD = 'net use /y "X:" "\\\\%SMB_URL%\\IOT-Release\\'+ SERVICE +'" /u:"GORILLASCIENCE\\%SMB_USERNAME%" %SMB_PASSWORD%'
+
+                run(winCMD)
+                projPath = os.path.join('X:\\','build' , PROJECT, VERSION, 'win-x86_64')
+                if not (os.path.isdir(projPath)):
+                    os.makedirs(projPath, mode=0o755, exist_ok=True)
+                shutil.copy2(rootPath.joinpath(SERVICE+'.zip'), projPath)
+                print('copy file ', projPath)
+
+                # compatibility
+                '''
+                qtAssignPath = os.path.join('X:\\' ,PROJECT, 'win-x86_64')
+                if not (os.path.isdir(qtAssignPath)):
+                    os.makedirs(qtAssignPath, mode=0o755, exist_ok=True)
+                shutil.copy2(rootPath.joinpath(SERVICE+'.zip'), qtAssignPath)
+                print('copy file ', qtAssignPath)
+                '''
+            else:
+                print('Test build')
+                projPath = os.path.join('X:\\' ,PROJECT, 'win-x86_64')
+                winCMD = 'net use /y "X:" "\\\\%SMB_URL%\\IOT-Release\\ci\\Packaging\\'+ SERVICE +'" /u:"GORILLASCIENCE\\%SMB_USERNAME%" %SMB_PASSWORD%'
+
+                run(winCMD)
+                if not (os.path.isdir(projPath)):
+                    os.makedirs(projPath, mode=0o755, exist_ok=True)
+                shutil.copy2(rootPath.joinpath(SERVICE+'.zip'), projPath)
+                print('copy file ', projPath)
+                #run('net use "X:" /delete /y')
 
         else:
-            print('Test build')
-            projPath = os.path.join('X:\\' ,PROJECT, 'win-x86_64')
-            winCMD = 'net use /y "X:" "\\\\%SMB_URL%\\IOT-Release\\ci\\Packaging\\'+ SERVICE +'" /u:"GORILLASCIENCE\\%SMB_USERNAME%" %SMB_PASSWORD%'
-        
-        run(winCMD)
-        if not (os.path.isdir(projPath)):
+            print('linux-x86_64')
+            smbtmpPath = os.path.join(rootPath, 'smbtmp')
+            os.makedirs(smbtmpPath, mode=0o755, exist_ok=True)
+            os.chdir(rootPath)
+            if os.getenv('CI_COMMIT_TAG'):
+                print ('Release build')
+                regExpr(os.environ['CI_COMMIT_TAG'])
+                run('mount -t cifs //$SMB_URL/IOT-Release/'+ SERVICE +' smbtmp -o user=$SMB_USERNAME,iocharset=utf8,password=$SMB_PASSWORD')
+                projPath = os.path.join(smbtmpPath,'build' , PROJECT, VERSION, 'linux-x86_64')
+
+                #compatibility
+                '''
+                qtAssignPath = os.path.join(smbtmpPath, PROJECT, 'linux-x86_64')
+                os.makedirs(qtAssignPath,  mode=0o755, exist_ok=True)
+                print('copy file', qtAssignPath)
+                shutil.copy(rootPath.joinpath(SERVICE+'.tar.gz'),qtAssignPath)
+                '''
+            else:
+                print ('Test build')
+                run('mount -t cifs //$SMB_URL/IOT-Release/ci/Packaging/'+ SERVICE +' smbtmp -o user=$SMB_USERNAME,iocharset=utf8,password=$SMB_PASSWORD')
+                projPath = os.path.join(smbtmpPath, PROJECT, 'linux-x86_64')
+
             os.makedirs(projPath, mode=0o755, exist_ok=True)
-        shutil.copy2(rootPath.joinpath(SERVICE+'.zip'), projPath)
-        print('copy file ', projPath)
-        run('net use "X:" /delete /y')
-        
-    else:
-        print('linux-x86_64')
-        smbtmpPath = os.path.join(rootPath, 'smbtmp')
-        os.makedirs(smbtmpPath, mode=0o755, exist_ok=True)
-        os.chdir(rootPath)
-        if os.getenv('CI_COMMIT_TAG'):
-            print ('Release build')
-            regExpr(os.environ['CI_COMMIT_TAG'])
-            run('mount -t cifs //$SMB_URL/IOT-Release/'+ SERVICE +' smbtmp -o user=$SMB_USERNAME,iocharset=utf8,password=$SMB_PASSWORD')
-            projPath = os.path.join(smbtmpPath,'build' , PROJECT, VERSION, 'linux-x86_64')
-
-            #compatibility
-            #tmpPath = os.path.join(rootPath, 'tmp')
-            #run('mount -t cifs //$SMB_URL/IOT-Release/'+ SERVICE +' tmp -o user=$SMB_USERNAME,iocharset=utf8,password=$SMB_PASSWORD')
-            qtAssignPath = os.path.join(smbtmpPath, PROJECT, 'linux-x86_64')
-            os.makedirs(qtAssignPath,  mode=0o755, exist_ok=True)
-            print('copy file', qtAssignPath)
-            shutil.copy(rootPath.joinpath(SERVICE+'.tar.gz'),qtAssignPath)
-            #run('umount tmp')
-            #print('remove tmpPath')
-            #shutil.rmtree(tmpPath)
-            
+            print('copy file ',projPath)
+            shutil.copy(rootPath.joinpath(SERVICE+'.tar.gz'),projPath)
+            #run('umount smbtmp')
+            #print('remove smbtmpPath')
+            #shutil.rmtree(smbtmpPath)
+               
+    except Exception as e:
+        print("Error:  %s" % str(e))
+        raise SystemExit(-1)
+    finally:
+        if os.name == 'nt':
+            run('net use "X:" /delete /y')
         else:
-            print ('Test build')
-            run('mount -t cifs //$SMB_URL/IOT-Release/ci/Packaging/'+ SERVICE +' smbtmp -o user=$SMB_USERNAME,iocharset=utf8,password=$SMB_PASSWORD')
-            projPath = os.path.join(smbtmpPath, PROJECT, 'linux-x86_64')
-        
-        os.makedirs(projPath, mode=0o755, exist_ok=True)
-        print('copy file ',projPath)    
-        shutil.copy(rootPath.joinpath(SERVICE+'.tar.gz'),projPath)
-        run('umount smbtmp')
-        print('remove smbtmpPath')
-        shutil.rmtree(smbtmpPath)
-        #os.system('rm -rf smbtmp/')
+            run('umount smbtmp')
+            print('remove smbtmpPath')
+            shutil.rmtree(smbtmpPath)
 
 def getProject(argv):
     global PROJECT
     PROJECT = 'std'
-    if len(argv) > 1 :
-        if str(argv[1]) == 'bi' or str(argv[1]) == 'telstra':
-            PROJECT = str(argv[1])
+    if len(argv) == 1 :
+        if str(argv[0]) == 'bi' or str(argv[0]) == 'telstra':
+            PROJECT = str(argv[0])
                    
     print('Project ',PROJECT)
 
@@ -132,7 +142,7 @@ def regExpr(s):
         
     else:
         print('Version Format Error')
-        raise SystemExit()
+        raise SystemExit(-1)
 
 def main(argv):
     global SERVICE
@@ -142,8 +152,9 @@ def main(argv):
     else :
         print ('SERVICE_NAME Not Found')
         raise SystemExit(-1)
-    getProject(sys.argv) 
+    getProject(argv) 
     packaging()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
